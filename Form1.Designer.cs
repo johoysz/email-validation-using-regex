5ce39977ca34827677c504145df65d0457b90b25
johoysz <120638033+johoysz@users.noreply.github.com>
2023-06-08 17:04:10 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Buangjug_SF1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTickets = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblTickets = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.lblNameError = new System.Windows.Forms.Label();
+            this.lblEmailError = new System.Windows.Forms.Label();
+            this.lblTicketsError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(102, 36);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(157, 26);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(102, 84);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(157, 26);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // txtTickets
+            // 
+            this.txtTickets.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTickets.Location = new System.Drawing.Point(102, 136);
+            this.txtTickets.Name = "txtTickets";
+            this.txtTickets.Size = new System.Drawing.Size(157, 26);
+            this.txtTickets.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(46, 42);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(47, 16);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(46, 90);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(44, 16);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblTickets
+            // 
+            this.lblTickets.AutoSize = true;
+            this.lblTickets.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTickets.Location = new System.Drawing.Point(39, 142);
+            this.lblTickets.Name = "lblTickets";
+            this.lblTickets.Size = new System.Drawing.Size(54, 16);
+            this.lblTickets.TabIndex = 5;
+            this.lblTickets.Text = "Tickets:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(80, 245);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(163, 51);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // comboBox
+            // 
+            this.comboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(50, 193);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(221, 28);
+            this.comboBox.TabIndex = 7;
+            // 
+            // lblNameError
+            // 
+            this.lblNameError.AutoSize = true;
+            this.lblNameError.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNameError.ForeColor = System.Drawing.Color.Red;
+            this.lblNameError.Location = new System.Drawing.Point(108, 67);
+            this.lblNameError.Name = "lblNameError";
+            this.lblNameError.Size = new System.Drawing.Size(0, 16);
+            this.lblNameError.TabIndex = 8;
+            // 
+            // lblEmailError
+            // 
+            this.lblEmailError.AutoSize = true;
+            this.lblEmailError.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmailError.ForeColor = System.Drawing.Color.Red;
+            this.lblEmailError.Location = new System.Drawing.Point(108, 113);
+            this.lblEmailError.Name = "lblEmailError";
+            this.lblEmailError.Size = new System.Drawing.Size(0, 16);
+            this.lblEmailError.TabIndex = 9;
+            // 
+            // lblTicketsError
+            // 
+            this.lblTicketsError.AutoSize = true;
+            this.lblTicketsError.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTicketsError.ForeColor = System.Drawing.Color.Red;
+            this.lblTicketsError.Location = new System.Drawing.Point(108, 165);
+            this.lblTicketsError.Name = "lblTicketsError";
+            this.lblTicketsError.Size = new System.Drawing.Size(0, 16);
+            this.lblTicketsError.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 339);
+            this.Controls.Add(this.lblTicketsError);
+            this.Controls.Add(this.lblEmailError);
+            this.Controls.Add(this.lblNameError);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblTickets);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtTickets);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Event Registration Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTickets;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblTickets;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label lblNameError;
+        private System.Windows.Forms.Label lblEmailError;
+        private System.Windows.Forms.Label lblTicketsError;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace Buangjug_SF1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void btnSubmit_Click(object sender, EventArgs e) {
+            Regex emailValid = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", RegexOptions.IgnoreCase);
+            bool isConverted = int.TryParse(txtTickets.Text, out int ticket);
+            try
+            {
+                if (string.IsNullOrEmpty(txtName.Text)) { lblNameError.Text = "*Enter your name"; } //temporary validation (1)
+                else { lblNameError.Text = string.Empty; } // (2)
+                if (!emailValid.IsMatch(txtEmail.Text)) { lblEmailError.Text = "*Invalid Email"; }
+                else { lblEmailError.Text = string.Empty; }
+                int.Parse(txtTickets.Text); 
+                if(isConverted) { lblTicketsError.Text = string.Empty; }
+            }
+            catch (FormatException)
+            {
+                lblTicketsError.Text = "*Positive Number Only";
+            }
+            if(!string.IsNullOrEmpty(txtName.Text) && emailValid.IsMatch(txtEmail.Text) && isConverted && ticket > 0) {
+                MessageBox.Show("Registration Confirm");
+                txtName.Text = string.Empty; txtEmail.Text = string.Empty; txtTickets.Text = string.Empty;
+                lblEmailError.Text = string.Empty; lblTicketsError.Text = string.Empty; comboBox.Text = string.Empty;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+            comboBox.Items.Add("Event 1"); comboBox.Items.Add("Event 2"); comboBox.Items.Add("Event 3");
+        }
+    }
+}
